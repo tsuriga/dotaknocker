4632291628f2f7d11f07f05396e4ea6363657a3c
tsuriga <royaltroyka@gmail.com>
2014-04-16 08:48:54 +0300

    Add Firebase and Dota API query functionalities

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-$(function () {
-    var viewModel = {
-        search: function (e) {
-            $.get(
-                $(e).attr('action'),
-                $(e).serialize(),
-                function (data) {
-                    $('.result').html(data);
-                }
-            );
-        }
-    };
-});
+var viewModel = {
+    search: function (e) {
+        $.get(
+            $(e).attr('action'),
+            $(e).serialize(),
+            function (data) {
+                $('.result').html(data);
+            }
+        );
+
+        return false;
+    }
+};
+
+ko.applyBindings(viewModel);
